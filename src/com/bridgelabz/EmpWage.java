75d17c73caeb8cea62b5b97e68a7170aa7370d44
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-08 20:58:52 +0530

    Added : Part time present

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,19 +1,24 @@
 package com.bridgelabz;
 
 class Employee {
-	static final int IS_PRESENT = 1;
+	static final int IS_FULL_TIME_PRESENT = 1;
+	static final int IS_PART_TIME_PRESENT = 2;
 	static final int WAGE_PER_HR = 20;
 	int workingHrs;
 	int salary;
-	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
 	/**
-	 * This is employee Attendance method to check whether He is present or absent
+	 * This is employee Attendance method to check whether He is full time present
+	 * or part time present or absent
 	 */
 	public void employeeAttendance() {
-		if (empCheck == IS_PRESENT) {
-			System.out.println("Present");
+		if (empCheck == IS_FULL_TIME_PRESENT) {
+			System.out.println("Full Time Present");
 			workingHrs = 8;
+		} else if (empCheck == IS_PART_TIME_PRESENT) {
+			System.out.println("Part Time present");
+			workingHrs = 4;
 		} else {
 			System.out.println("Absent");
 			workingHrs = 0;
@@ -38,6 +43,6 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wage");
 		Employee employee = new Employee();
 		employee.empSalary();
-		System.out.println(employee.salary);
+		System.out.println("salary : " + employee.salary + "rs");
 	}
 }
