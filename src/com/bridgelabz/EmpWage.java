a7101d576e2dab1eb6aa2ec6c209e178cfba2dd0
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-08 20:21:20 +0530

    Added : Welcome message

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,6 +1,10 @@
 package com.bridgelabz;
 
 public class EmpWage {
+	/**
+	 * @param args
+	 * This is Main method having Welcome message
+	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
 	}
