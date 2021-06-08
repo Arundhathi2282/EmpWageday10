d8a86e77ac432eba2eb1ebfadfdca2e3b3983b44
Arundhathi2282 <arundhathikuricheti@gmail.com>
2021-06-08 20:37:45 +0530

    Added : Employee Attendance

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,5 +1,21 @@
 package com.bridgelabz;
 
+class Employee {
+	static final int IS_PRESENT = 1;
+	int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+	
+	/**
+	 * This is employee Attendance method to check
+	 * whether He is present or absent
+	 */
+	public void employeeAttendance() {
+		if (empCheck == IS_PRESENT) {
+			System.out.println("Present");
+		}else {
+			System.out.println("Absent");
+		}
+	}
+}
 public class EmpWage {
 	/**
 	 * @param args
@@ -7,5 +23,7 @@ public class EmpWage {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage");
+		Employee employee = new Employee();
+		employee.employeeAttendance();
 	}
 }
